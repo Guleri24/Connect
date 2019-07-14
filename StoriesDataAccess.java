9d88efcc308f214b23ad4a04eeb6be0922fdad38
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-07-14 19:12:16 +0530

    DataAccess class Added
    DataAccess calls contain all set of the data which is accessed using the specific methods

--- StoriesDataAccess.java
+++ StoriesDataAccess.java
@@ -1,34 +1,46 @@
+
 package Guleri;
 
- class StoriesDataAccess {
-    void OvercomingTheMonsterAccess(){
+class StoriesDataAccess {
+    StoriesData DataAccess = new StoriesData();
+
+    void OvercomingTheMonsterAccess() {
         System.out.println("Choose the story to read :");
+        DataAccess.DataOvercomingTheMonster();
     }
-    void RagsToRichesAccess(){
+
+    void RagsToRichesAccess() {
         System.out.println("Choose the story to read :");
+        DataAccess.DataRagsToRiches();
 
     }
-    void TheQuestAccess(){
+
+    void TheQuestAccess() {
         System.out.println("Choose the story to read :");
-
+        DataAccess.DataTheQuest();
     }
-    void VoyageAndReturnAccess(){
+
+    void VoyageAndReturnAccess() {
         System.out.println("Choose the story to read :");
-
+        DataAccess.DataVoyageAndReturn();
     }
-    void ComedyAccess(){
+
+    void ComedyAccess() {
         System.out.println("Choose the story to read :");
-
+        DataAccess.DataComedy();
     }
-    void TragedyAccess(){
+
+    void TragedyAccess() {
         System.out.println("Choose the story to read :");
-
+        DataAccess.DataTragedy();
     }
-    void RebirthAccess(){
+
+    void RebirthAccess() {
         System.out.println("Choose the story to read :");
-
+        DataAccess.DataRebirth();
     }
-    void RandomAccess(){
 
+    void RandomAccess() {
+        DataAccess.DataRandom();
     }
 }
